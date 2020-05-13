fa1d03014289fde1276521facb231e7f4de100f7
Babis Babalis <babisbabalis@gmail.com>
2020-05-13 16:45:10 +0300

    grid for vrp has been created

---
+++ grid_vrp.py
@@ -0,0 +1,118 @@
+"""Vehicles Routing Problem (VRP)."""
+
+from __future__ import print_function
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+import pdb
+import time
+
+
+
+def create_equal_distance():
+    data = {}
+    points_matrix = create_grid_points()
+    data['distance_matrix'] = calculate_grid_distances(points_matrix)
+    data['num_vehicles'] = 4
+    data['depot'] = 0
+    return data
+
+
+def create_grid_points():
+    points_matrix = []
+    for i in range(0,10):
+        for j in range(0,10):
+            points_matrix.append((i, j))
+    print(points_matrix)
+    print("points matrix just above!\n")
+    return points_matrix
+
+def calculate_grid_distances(points_matrix):
+    distance_matrix = []
+    row_matrix = []
+    for point_a in points_matrix:
+        for point_b in points_matrix:
+            dst = distance.euclidean(point_a, point_b)
+            row_matrix.append(dst)
+        distance_matrix.append(row_matrix)
+        row_matrix = []
+    #print(distance_matrix)
+    #print("distance matrix just above!\n")
+    return distance_matrix
+
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    max_route_distance = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print('Maximum of the route distances: {}m'.format(max_route_distance))
+
+
+
+
+def main():
+    """Solve the CVRP problem."""
+    # Instantiate the data problem.
+    data = create_equal_distance()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    dimension_name = 'Distance'
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == '__main__':
+    main()
